f7b6312796742275b5512f9ec5258ee2e7b94e53
Mical Juarez <mical.juarez@gmail.com>
2017-05-01 02:03:43 -0500

    can now utilize the search input and button to log information

--- gov-info-api.js
+++ gov-info-api.js
@@ -2,26 +2,38 @@
 let authKey = "AIzaSyDI6y22UiWcVtePvg5EMGB0ifIiV4TKgQ4"
 
 // variables to use for the search
-let searchAddress = ""
+let searchByAddress = ""
 
 // query url to use for api call
-let baseQueryURL = "https://www.googleapis.com/civicinfo/v2/representatives?key=" + authKey + "&address=austin+texas"
+let baseQueryURL = "https://www.googleapis.com/civicinfo/v2/representatives?key=" + authKey + "&address="
 
 // function to return information based on address
-function runQuery(queryURL) {
+function runQuery(search) {
   $.ajax({
-    url: queryURL,
+    url: search,
     method: "GET"
   }).done(function(repData) {
 
-    console.log(repData)
+    console.log(repData);
 
     for (i = 3; i < repData.offices.length; i++) {
       namePos = repData.offices[i].officialIndices[0]
-      console.log(repData.offices[i].name + ": " + repData.officials[namePos].name)
+      console.log(repData.offices[i].name + ": " + repData.officials[namePos].name);
     }
 
-  })
+  });
 }
 
-runQuery(baseQueryURL)
+// click handler for searching
+$("#searchBtn").on("click", function() {
+
+  // take the search from user input
+  searchByAddress = $("#adSearch").val().trim();
+
+  // combine search address with the base query URL
+  queryURL = baseQueryURL + searchByAddress;
+
+  // run the functino to search api
+  runQuery(queryURL);
+
+});
