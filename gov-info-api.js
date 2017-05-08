8bd8dd7fe3e7898fea727e7df57e8b89f7389e50
Mical Juarez <mical.juarez@gmail.com>
2017-05-08 18:35:46 -0500

    successfully appending results in the rep panel

--- gov-info-api.js
+++ gov-info-api.js
@@ -7,6 +7,9 @@ let searchByAddress = ""
 // query url to use for api call
 let baseQueryURL = "https://www.googleapis.com/civicinfo/v2/representatives?key=" + authKey + "&address="
 
+// counter to give each resulting div a unique identifier
+let resultCounter = 1
+
 // function to return information based on address
 function runQuery(search) {
   $.ajax({
@@ -14,11 +17,19 @@ function runQuery(search) {
     method: "GET"
   }).done(function(repData) {
 
+    // console log the result object for error checking
     console.log(repData);
 
     for (i = 3; i < repData.offices.length; i++) {
+
+      // variable to keep position of person in offices name
       namePos = repData.offices[i].officialIndices[0]
+      // console log data for error checking
       console.log(repData.offices[i].name + ": " + repData.officials[namePos].name);
+
+      $('#state-officials-0').append('<h3 id="result"' + resultCounter + '>' + repData.offices[i].name + ": " + repData.officials[namePos].name + '</h3></div>');
+
+      resultCounter++;
     }
 
   });
